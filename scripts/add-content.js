e945fa2edd5453a4d04da56da4e15005f07e5837
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-07-10 02:20:04 +0100

    Added greetging to the web page

--- scripts/add-content.js
+++ scripts/add-content.js
@@ -4,7 +4,7 @@ var greeting;
 
 if (hourNow > 18) {
   greeting = 'Good evening!'
-} else if (howNow > 12) {
+} else if (hourNow > 12) {
   greeting = 'Good afternoon!';
 } else if (hourNow > 0) {
   greeting =  'Good morning!';
@@ -12,4 +12,4 @@ if (hourNow > 18) {
   greeting = 'Welcome!';
 }
 // I need to look into this last line
-document.write('<h3>' + greeting + '</h3>');
+document.write('<h1>' + greeting + '</h1>');
